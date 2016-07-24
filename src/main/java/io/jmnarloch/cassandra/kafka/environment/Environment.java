6191369839cd1394662af77619ad735a34259b9f
Jakub Narloch <jmnarloch@gmail.com>
2016-07-25 00:10:17 +0200

    Added copyrights

--- src/main/java/io/jmnarloch/cassandra/kafka/environment/Environment.java
+++ src/main/java/io/jmnarloch/cassandra/kafka/environment/Environment.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2015-2016 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jmnarloch.cassandra.kafka.environment;
 
 import io.jmnarloch.cassandra.kafka.exception.EnvironmentException;
@@ -20,7 +35,7 @@ public class Environment {
         return (String) properties.get(key);
     }
 
-    public Environment filter(String prefix) {
+    public Environment prefix(String prefix) {
         final Properties properties = new Properties();
         for(Map.Entry<Object, Object> entry : properties.entrySet()) {
             if(entry.getKey() instanceof String && ((String) entry.getKey()).startsWith(prefix)) {
--- src/main/java/io/jmnarloch/cassandra/kafka/trigger/CommitLogTrigger.java
+++ src/main/java/io/jmnarloch/cassandra/kafka/trigger/CommitLogTrigger.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2015-2016 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jmnarloch.cassandra.kafka.trigger;
 
 import io.jmnarloch.cassandra.kafka.api.CommitLog;
@@ -27,11 +42,11 @@ public class CommitLogTrigger implements ITrigger {
             final String key = update.metadata().getKeyValidator().getString(update.partitionKey().getKey());
             final UnfilteredRowIterator rows = update.unfilteredIterator();
             while (rows.hasNext()) {
-                final Unfiltered unfiltered = rows.next();
-                if (shouldSkip(unfiltered)) {
+                final Unfiltered unfilteredRow = rows.next();
+                if (shouldSkip(unfilteredRow)) {
                     continue;
                 }
-                final Row row = update.getRow((Clustering) unfiltered);
+                final Row row = update.getRow((Clustering) unfilteredRow);
                 final byte[] data = formatter.format(row);
                 commitLog.commit(key, data);
             }
